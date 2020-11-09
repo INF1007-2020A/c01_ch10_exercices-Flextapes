a0465dbfa9a3011cb43fab4e9ddaad82789eee89
carl salem <71038407+Flextapes@users.noreply.github.com>
2020-11-09 14:20:26 -0500

    j'aime pas main

--- exercice.py
+++ exercice.py
@@ -4,22 +4,30 @@
 
 # TODO: Importez vos modules ici
 import numpy as np
-
+import math
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.linespace(-1.3, 2.5, 64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    #copier de stackOverflow
+    array = np.zeros(len(cartesian_coordinates), 2)
+    
+    for i in range(len(cartesian_coordinates)):
+        rho = np.sqrt(cartesian_coordinates[i][0]**2 + cartesian_coordinates[i][1]**2)
+        phi = np.arctan2(cartesian_coordinates[i][1], cartesian_coordinates[i][0])
+        polar_coordinates = (rho, phi) 
+    
+    return polar_coordinates
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+    #copier du prof
+    return np.abs(values - number).argim()
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
     pass
